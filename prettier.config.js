cb5c2e097f21554b5f0db41ef1ee6a78cd2c63fe
Luuk <me@luuk.network>
2024-09-12 14:41:56 +0200

    Added prettier

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+	printWidth: 100,
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	bracketSameLine: false,
+	vueIndentScriptAndStyle: true,
+	singleAttributePerLine: true
+};
+
+export default config;
