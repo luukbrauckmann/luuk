6c5049a4e9107e658dbb4010a9779e07a430e97b
Luuk <me@luuk.network>
2024-09-17 16:04:45 +0200

    Add cloudflarte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
 	output: 'hybrid',
+	adapter: cloudflare(),
 	devToolbar: {
 		enabled: false
 	}
