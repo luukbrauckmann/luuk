61bf9e4a90060d6be0e11583b9696e012c2c819f
Luuk <me@luuk.network>
2024-10-03 21:27:23 +0200

    Quick fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import graphql from '@rollup/plugin-graphql';
 
 export default defineConfig({
 	output: 'hybrid',
 	adapter: cloudflare(),
+  image: {
+      // cloudflare is not supported by the Astro image service
+      // @see https://docs.astro.build/en/guides/images/#configure-no-op-passthrough-service
+      service: passthroughImageService()
+    },
 	vite: {
     plugins: [
       graphql(),
--- src/env.d.ts
+++ src/env.d.ts
@@ -4,3 +4,9 @@ interface ImportMetaEnv {
   readonly DATOCMS_URL: string
   readonly DATOCMS_API_KEY: string;
 }
+
+declare module '*.query.graphql' {
+  import { DocumentNode } from 'graphql';
+  const value: DocumentNode;
+  export = value;
+}
