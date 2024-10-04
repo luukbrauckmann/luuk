29cb425bc3f8ed6b9d0706b6fe0581c7f2976d31
Luuk <me@luuk.network>
2024-10-04 19:09:51 +0200

    Removed plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import graphql from '@rollup/plugin-graphql';
-import autoImport from 'unplugin-auto-import/vite';
 
 export default defineConfig({
 	output: 'hybrid',
@@ -12,17 +11,7 @@ export default defineConfig({
 		}
 	}),
 	vite: {
-		plugins: [
-			graphql(),
-			autoImport({
-				imports: [
-					{
-						'./src/scripts/logger': ['logger']
-					}
-				],
-				dts: './src/auto-imports.d.ts'
-			})
-		]
+		plugins: [graphql()]
 	},
 	devToolbar: {
 		enabled: false
