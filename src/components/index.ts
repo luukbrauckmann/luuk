deebf00ab25db5f51b485ce4f4b525a45c7156f0
Luuk <me@luuk.network>
2024-10-04 11:34:02 +0200

    Added logo

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Image } from './Image/Image.astro';
 export { default as DecoratedText } from './DecoratedText.astro';
 export { default as LocaleSelector } from './LocaleSelector.astro';
+export { default as Logo } from './Logo.astro';
