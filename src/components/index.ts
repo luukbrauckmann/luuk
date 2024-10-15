07ddc853d463aa748c202f25b86c1ca31c3d23a5
Luuk <me@luuk.network>
2024-10-15 15:46:19 +0200

    hmrhiothmrh

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
-export { default as Image } from './Image/Image.astro';
 export { default as DecoratedText } from './DecoratedText.astro';
+export { default as Image } from './Image.astro';
 export { default as LocaleSelector } from './LocaleSelector.astro';
 export { default as Logo } from './Logo.astro';
+export { default as ThemeInitializer } from './ThemeInitializer.astro';
+export { default as ThemeSelector } from './ThemeSelector.astro';
